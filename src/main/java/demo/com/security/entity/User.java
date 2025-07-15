9e99d3af13cbe9b16d73c93a08ed5fc76e342ac4
Biel <gkortschinski@gmail.com>
2025-07-15 16:29:04 -0300

    Criação do User Entity

--- src/main/java/demo/com/security/entity/User.java
+++ src/main/java/demo/com/security/entity/User.java
@@ -1,4 +1,23 @@
 package demo.com.security.entity;
 
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.sql.Timestamp;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collation = "user_entity")
 public class User {
+
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private String document;
+    private String password;
+    private Timestamp created;
+    private Timestamp updated;
 }
